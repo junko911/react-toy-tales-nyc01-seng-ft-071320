d02bb63159d8f72277391bcd6f70c5d31d5ef573
junko911 <junkotahara911@gmail.com>
2020-09-19 22:02:07 -0400

    RRender toycards

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ToyCard from './ToyCard'
 
-const ToyContainer = () => {
+const ToyContainer = (props) => {
+
+  const toyCards = () => props.toys.map(toy => <ToyCard toy={toy}/>)
+
   return(
     <div id="toy-collection">
-      {/* Render the collection of ToyCards */}
+      {toyCards()}
     </div>
   );
 }
